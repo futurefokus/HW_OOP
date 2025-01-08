561eaec0665a6bb030d525e995f91cac6d850f5a
futurefokus <futurefokus24@gmail.com>
2025-01-08 21:16:16 +0500

    Update Product.java

--- src/InventoryManagement/Product.java
+++ src/InventoryManagement/Product.java
@@ -1,4 +1,57 @@
 package InventoryManagement;
 
+import java.util.*;
+
 public class Product {
+    private String name;
+    private int productId;
+    private double price;
+
+    public Product(String name, int productId, double price) {
+        this.name = name;
+        this.productId = productId;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{name='" + name + "', productId=" + productId + ", price=" + price + "}";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return productId == product.productId && Double.compare(product.price, price) == 0 && Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, productId, price);
+    }
 }
